9d85f13f5e83132df14ac5db75eb8410e239a3da
André <Andr@192.168.1.66>
2020-03-10 12:18:37 +0100

    WIP3

--- src/controller/TimeReportController.java
+++ src/controller/TimeReportController.java
@@ -1,10 +1,10 @@
 package controller;
 
-import java.sql.Date;  //oklart evt util
 import java.io.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpSession;
 
 import baseblocksystem.servletBase;
 import database.ActivityReport;
+import database.ActivitySubType;
+import database.ActivityType;
 import database.DatabaseService;
 import database.TimeReport;
 import database.User;
@@ -24,173 +26,230 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TimeReportController
  * 
- * A xx page. 
+ * A xx page.
  * 
  * Description of the class.
  * 
- * @author Ferit B�lezek ( Enter name if you've messed around with this file ;) )
+ * @author Ferit B�lezek ( Enter name if you've messed around with this file ;)
+ *         )
  * @version 1.0
  * 
  */
-
 
 // wtf is this i try to fix but it never work
 
 @WebServlet("/TimeReportPage")
 public class TimeReportController extends servletBase {
-	
+
 	DatabaseService dbService;
 
-	public TimeReportController( ) {
+	public TimeReportController() {
 		super();
 		try {
-			DatabaseService dbService = new DatabaseService();
+			this.dbService = new DatabaseService();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	
-/*private ActivityReport createActivityReport(int activityTypeId, int activitySubTypeId, Date date, int minutes, int userId, int projectId ) {
-	
-	
-	return null;
- }
-*/
-	
-	   @Override
-	    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			PrintWriter out = resp.getWriter();
-			out.println(getPageIntro());
-			
+	/*
+	 * 
+	 * private ActivityReport createActivityReport(int activityTypeId, int
+	 * activitySubTypeId, LocalDate date, int minutes, int userId, int projectId ) {
+	 * 
+	 * 
+	 * finnstidrapport annars skapa tidrapport skapa aktivitetsrapport i
+	 * vilketfallsom minutes 1440 annars exceptions
+	 * 
+	 * return null; }
+	 */
 
-			String activityType = req.getParameter("activity"); // get activity
-			String subType = req.getParameter("subType"); // get activity subtype
-			String timeSpent = req.getParameter("timeSpent"); //get timespent (integer.parseInt()?)
-			
-		
-			
-		//	System.out.println(activityType);
-		//	System.out.println(subType);
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println(getPageIntro());
+		try {
+			out.println(getTableAllTimereports());
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
-		//	System.out.println(timeSpent);
-	        
-	        
+		String activityType = req.getParameter("activity"); // get activity
+		String subType = req.getParameter("subType"); // get activity subtype
+		String timeSpent = req.getParameter("timeSpent"); // get timespent (integer.parseInt()?)
 		
+		String timeReportId = req.getParameter("timeReportId");
+		System.out.println(timeReportId);
+		// System.out.println(activityType);
+		// System.out.println(subType);
+
+		// System.out.println(timeSpent);
+		
+		if(!timeReportId.equals(0) && timeReportId != null) {
+			int id = Integer.parseInt(timeReportId);
 			try {
-				out.println(getTableAllTimereports());
+				out.println(getActivityReports(id));
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
-	    }
-	   
-	   private String getTableAllTimereports() throws SQLException {
-			String html ="<table width=\"400\" border=\"2\">\r\n";
-	   
-						User currentUser = new User(1, "a", "a", false);//getLoggedInUser(req); //ÄR DETTA RÄTT?
-						System.out.println(dbService);
-						List<TimeReport> userTimeReports = dbService.getTimeReportsByUser(1); //Get all timereports for logged in user
-					
-					for(TimeReport tr : userTimeReports) {
-						
-						int timeReportTotalTime = getTotalTimeReportTime(tr);
-						String signed;
-						
-						
-						
-						if(tr.isSigned()) {  											//get isSigned or not
-							signed = "Signerad";
-						}
-						else {
-							signed = "Ej signerad";
-						}
-						
-						
-						html += "<tr>\r\n" + 
-					"<td>" + tr.getWeek() + "</td>\r\n" +  							//set values into HTML
-					"<td>" + timeReportTotalTime +"d</td>\r\n" +
-					"<td>" + signed + "</td>\r\n" +
-					"<td> <input class=\"submitBtn\" type=\"submit\" value=\"Välj tidrapport\">\r\n" + 
-					"<td> <input class=\"submitBtn\" type=\"submit\" value=\"Tabort\">\r\n" + 			//TODO: remove knapp
-					"</td>\r\n" +  
-					"</tr>\r\n";
-					}
-					
-					
-					
-					
-					 html += "</tr>\r\n" + 
-					"</table>"; 			//END HTML
-					
-					
-					return html;
-					
-					
+		}
+
 	}
+
+	private String getActivityReports(int timeReportId) throws SQLException {
+
+		String html = "<table width=\"600\" border=\"2\">\r\n" + "<tr>\r\n" + "<td> Datum </td>\r\n"
+				+ "<td> Aktivitetstyp </td>\r\n" + "<td> Subtyp </td>\r\n" + "<td> Minuter </td>\r\n"
+				+ "<td> Ta bort aktivitetsrapport </td>\r\n";
+
+		List<ActivityReport> activityReports = dbService.getActivityReports(timeReportId);
+		List<ActivityType> activityTypes = dbService.getActivityTypes();
+		List<ActivitySubType> activitySubTypes = dbService.getActivitySubTypes();
+
+		String activityType;
+		String activitySubType;
+
+		for (ActivityReport aReport : activityReports) {
 			
-	   private int getTotalTimeReportTime(TimeReport tr) throws SQLException {
-		   
-		   int totalTime = 0;
-		   
-		   List<ActivityReport> activitiesInTimeReport = dbService.getActivityReports(tr.getTimeReportId());
+			activityType = getActivityType(aReport, activityTypes);
+			activitySubType = getActivitySubType(aReport, activitySubTypes);
 			
-			for(ActivityReport ar : activitiesInTimeReport) { //calculate total time from all activity reports inside this timeReport
-				totalTime += ar.getMinutes();
+
+			html +=   "<tr>\r\n" 
+					+ "<td>" + aReport.getReportDate().toString() + "</td>\r\n"
+					+ "<td>" + activityType + "</td>\r\n" 
+					+ "<td>" + activitySubType + "</td>\r\n" 
+					+ "<td>" + aReport.getMinutes() + "</td>\r\n"  //TODO: ska gå att ändra i tabellen om rapporten inte är signerad
+					+  "<td> <form method=\"get\"> <button name=\"activityReportId\" type=\"submit\" value=\"" + aReport.getActivityReportId() + "\"> Ta bort </button>  </form> \r\n";	//TODO: ska gå att tabort aktivitetsrapport om den inte är signerad
+		}		
+
+		html += "</tr>\r\n" + "</table>"; // END HTML
+
+		return html;
+	}
+	
+	
+
+	private String getActivityType(ActivityReport activityReport, List<ActivityType> typeList) throws SQLException {
+		
+		   for(ActivityType aType: typeList) { //Get activity type for current activity report
+			   
+			   if(aType.getActivityTypeId() == activityReport.getActivityTypeId()) {
+				   return aType.getType();
+			   }
+		   }
+		   
+		   return ""; //TODO: EXCEPTION?? Borde alltid hitta en type.
+			
+		}
+	
+	private String getActivitySubType(ActivityReport activityReport, List<ActivitySubType> subTypeList) throws SQLException{
+		
+		for (ActivitySubType aSubType : subTypeList) { // Get activity type for current activity report
+				
+			if (aSubType.getActivitySubTypeId() == activityReport.getActivitySubTypeId()) { //TODO: Kanske fel, kolla i DB
+				
+				return aSubType.getSubType();
 			}
-		   
-			return totalTime;
-	   }
-	   
-		private String activityReportForm() {
-				return 	"<link rel=\"stylesheet\" type=\"text/css\" href=\"StyleSheets/SessionController.css\">"+
-						" <form id=\"filter_form\" onsubmit=\"checkInput()\">\r\n" + 
-						"                 Aktivitetstyp\r\n" + 
-						"                <div id=\"activity_picker\">\r\n" + 
-						"                    <select id=\"act_picker\" name=\"activity\" form=\"filter_form\">\r\n" + 
-						"                        <option value=\"SDP\">SDP</option>\r\n" + 
-						"                        <option value=\"SRS\">SRS</option>\r\n" + 
-						"                        <option value=\"SVVS\">SVVS</option>\r\n" + 
-						"                        <option value=\"STLDD\">STLDD</option>\r\n" + 
-						"                        <option value=\"SVVI\">SVVI</option>\r\n" + 
-						"                        <option value=\"SDDD\">SDDD</option>\r\n" + 
-						"                        <option value=\"SVVR\">SVVR</option>\r\n" + 
-						"                        <option value=\"SSD\">SSD</option>\r\n" + 
-						"                        <option value=\"Slutrapport\">Slutrapport</option>\r\n" + 
-						"                        <option value=\"Funktionstest\">Funktionstest</option>\r\n" + 
-						"                        <option value=\"Systemtest\">Systemtest</option>\r\n" + 
-						"                        <option value=\"Regressionstest\">Regressionstest</option>\r\n" + 
-						"                        <option value=\"Mote\">Möte</option>\r\n" + 
-						"                        <option value=\"Foreläsning\">Föreläsning</option>\r\n" + 
-						"                        <option value=\"Ovning\">Övning</option>\r\n" + 
-						"                        <option value=\"Terminalovning\">Terminalövning</option>\r\n" + 
-						"                        <option value=\"Sjalvstudier\">Självstudier</option>\r\n" + 
-						"                        <option value=\"Ovrigt\">Övrigt</option>\r\n" + 
-						"                      </select>\r\n" + 
-						"                </div>\r\n" + 
-						"            </div>\r\n" + 
-						"            <div>\r\n" + 
-						"                <p class=\"descriptors\">Aktivitet subtyp</p>\r\n" + 
-						"                <div id=\"activity_picker\">\r\n" + 
-						"                    <select id=\"act_picker\" name=\"subType\" form=\"filter_form\">\r\n" + 
-						"					     <option value=\"\"></option>\r\n" + 
-						"                        <option value=\"Utveckling\">Utveckling</option>\r\n" + 
-						"                        <option value=\"Omarbete\">Omarbete</option>\r\n" + 
-						"                        <option value=\"Informellgranskning\">Informellgranskning</option>\r\n" + 
-						"                        <option value=\"Formellgranskning\">Formellgranskning</option>\r\n" + 
-						"                      </select>\r\n" + 
-						"                </div>\r\n" + 
-						"            </div>\r\n" + 
-						"                <p class=\"descriptors\">Tid spenderad (i minuter) </p>\r\n" + 
-						"                <div id=\"activity_picker\">\r\n"+ 
-						"				</div>" + 
-						"              <input class=\"credentials_rect\" type=\"text\" id=\"timeSpent\" name=\"timeSpent\" pattern=\"^[0-9]*$\" title=\"Please enter numbers only.\" maxlength=\"4\" placeholder=\"Tid Spenderad\" required><br>\r\n" + 
-						"              <input class=\"submitBtn\" type=\"submit\" value=\"Skicka in\">\r\n" + 
-						"                </div>\r\n" + 
-						"              </form> ";
+		}
+		
+		return ""; //TODO: EXCEPTION?? Borde alltid hitta en subtype.
+	}
+
+	private String getTableAllTimereports() throws SQLException {
+		String html = "<table width=\"400\" border=\"2\">\r\n" 
+				+ "<tr>\r\n" 
+				+ "<td> week </td>\r\n"
+				+ "<td> timespent(minutes) </td>\r\n" 
+				+ "<td> Status </td>\r\n" 
+				+ "<td> Välj tidrapport </td>\r\n"
+				+ "<td> Ta bort tidrapport </td>\r\n"; //TODO: Ska ej gå att ta bort om den är signerad
+
+		User currentUser = new User(1, "a", "a", false);// TODO: getLoggedInUser(req); 
+		List<TimeReport> userTimeReports = dbService.getTimeReportsByUser(1); //TODO:  Get all timereports for logged in user
+
+		for (TimeReport tr : userTimeReports) {
+
+			int timeReportTotalTime = getTotalTimeReportTime(tr);
+			String signed;
+
+			if (tr.isSigned()) { // get isSigned or not
+				signed = "Signerad";
+			} else {
+				signed = "Ej signerad";
+			}
+
+			html += "<tr>\r\n" + "<td>" + tr.getWeek() + "</td>\r\n" + // set values into HTML
+					"<td>" + timeReportTotalTime + "</td>\r\n" + "<td>" + signed + "</td>\r\n"
+					+ "<td> <form method=\"get\"> <button name=\"timeReportId\" type=\"submit\" value=\"" + tr.getTimeReportId() 
+					+ "\"> Välj </button>  </form> \r\n" 
+					+ "<td> <input class=\"submitBtn\" type=\"submit\" value=\"Ta bortt\">\r\n"
+					+ "</td>\r\n" + "</tr>\r\n";
+
+		}
+
+		html += "</tr>\r\n" + "</table>"; // END HTML
+
+		return html;
+
+	}
+
+	private int getTotalTimeReportTime(TimeReport tr) throws SQLException {
+
+		int totalTime = 0;
+
+		List<ActivityReport> activitiesInTimeReport = dbService.getActivityReports(tr.getTimeReportId());
+
+		for (ActivityReport ar : activitiesInTimeReport) { // calculate total time from all activity reports inside this
+															// timeReport
+			totalTime += ar.getMinutes();
+		}
+		return totalTime;
+	}
+
+	private String activityReportForm() {
+		return "<link rel=\"stylesheet\" type=\"text/css\" href=\"StyleSheets/SessionController.css\">"
+				+ " <form id=\"filter_form\" onsubmit=\"checkInput()\">\r\n" + "                 Aktivitetstyp\r\n"
+				+ "                <div id=\"activity_picker\">\r\n"
+				+ "                    <select id=\"act_picker\" name=\"activity\" form=\"filter_form\">\r\n"
+				+ "                        <option value=\"SDP\">SDP</option>\r\n"
+				+ "                        <option value=\"SRS\">SRS</option>\r\n"
+				+ "                        <option value=\"SVVS\">SVVS</option>\r\n"
+				+ "                        <option value=\"STLDD\">STLDD</option>\r\n"
+				+ "                        <option value=\"SVVI\">SVVI</option>\r\n"
+				+ "                        <option value=\"SDDD\">SDDD</option>\r\n"
+				+ "                        <option value=\"SVVR\">SVVR</option>\r\n"
+				+ "                        <option value=\"SSD\">SSD</option>\r\n"
+				+ "                        <option value=\"Slutrapport\">Slutrapport</option>\r\n"
+				+ "                        <option value=\"Funktionstest\">Funktionstest</option>\r\n"
+				+ "                        <option value=\"Systemtest\">Systemtest</option>\r\n"
+				+ "                        <option value=\"Regressionstest\">Regressionstest</option>\r\n"
+				+ "                        <option value=\"Mote\">Möte</option>\r\n"
+				+ "                        <option value=\"Foreläsning\">Föreläsning</option>\r\n"
+				+ "                        <option value=\"Ovning\">Övning</option>\r\n"
+				+ "                        <option value=\"Terminalovning\">Terminalövning</option>\r\n"
+				+ "                        <option value=\"Sjalvstudier\">Självstudier</option>\r\n"
+				+ "                        <option value=\"Ovrigt\">Övrigt</option>\r\n"
+				+ "                      </select>\r\n" + "                </div>\r\n" + "            </div>\r\n"
+				+ "            <div>\r\n" + "                <p class=\"descriptors\">Aktivitet subtyp</p>\r\n"
+				+ "                <div id=\"activity_picker\">\r\n"
+				+ "                    <select id=\"act_picker\" name=\"subType\" form=\"filter_form\">\r\n"
+				+ "					     <option value=\"\"></option>\r\n"
+				+ "                        <option value=\"Utveckling\">Utveckling</option>\r\n"
+				+ "                        <option value=\"Omarbete\">Omarbete</option>\r\n"
+				+ "                        <option value=\"Informellgranskning\">Informellgranskning</option>\r\n"
+				+ "                        <option value=\"Formellgranskning\">Formellgranskning</option>\r\n"
+				+ "                      </select>\r\n" + "                </div>\r\n" + "            </div>\r\n"
+				+ "                <p class=\"descriptors\">Tid spenderad (i minuter) </p>\r\n"
+				+ "                <div id=\"activity_picker\">\r\n" + "				</div>"
+				+ "              <input class=\"credentials_rect\" type=\"text\" id=\"timeSpent\" name=\"timeSpent\" pattern=\"^[0-9]*$\" title=\"Please enter numbers only.\" maxlength=\"4\" placeholder=\"Tid Spenderad\" required><br>\r\n"
+				+ "              <input class=\"submitBtn\" type=\"submit\" value=\"Skicka in\">\r\n"
+				+ "                </div>\r\n" + "              </form> ";
+
+		// html += activitysubtype.getId...
 	}
 
 }
