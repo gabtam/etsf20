3780390417281ab730b08f866640b84aea8fe9fe
ch0815la-s <ch0815la-s@egsws1970.pc.lu.se>
2020-03-10 17:23:00 +0100

    Tyyyyp klar

--- src/controller/StatisticController.java
+++ src/controller/StatisticController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import baseblocksystem.servletBase;
+import database.ActivityType;
+import database.DatabaseService;
+import database.Role;
+import database.Statistic;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Servlet implementation class StatisticController
@@ -16,18 +30,278 @@ import baseblocksystem.servletBase;
  * 
  * Description of the class.
  * 
- * @author Ferit B�lezek ( Enter name if you've messed around with this file ;) )
+ * @author Ferit Blezek ( Enter name if you've messed around with this file ;) )
  * @version 1.0
  * 
  */
 
-@WebServlet("/StatisticsPage")
+@WebServlet("/statistics")
 public class StatisticController extends servletBase {
+	
+	
+	private DatabaseService dbService; // Temporary, will be replaced later.
+	
+	
+	public StatisticController() {
+		super();
+		try {
+			dbService = new DatabaseService();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private Statistic getUserStats(int userID, int projectID, LocalDate from, LocalDate to) throws Exception {
+		return dbService.getActivityStatistics(userID, projectID, from, to); // FIXME: how do we get projectId? Some hardcoded values for now.
+	}
+	
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println(getHeader());
+		
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String username = req.getParameter("username");
+		String from = req.getParameter("from");
+		String to = req.getParameter("to");
+		String activity = req.getParameter("activity");
+		String role = req.getParameter("role");
+		
+		
+		out.println("<body>");
+		
+		if (username == null || from == null || to == null || activity == null || role == null) {
+			out.println(statisticsPageForm(null));
+		} else {
+			try {
+				LocalDate fromDate = new SimpleDateFormat("dd-MMM-yyy").parse(from).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+				LocalDate toDate = new SimpleDateFormat("dd-MMM-yyy").parse(to).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+				
+				int weeks = (int) ChronoUnit.WEEKS.between(fromDate, toDate);
+				List<Statistic> stats = new ArrayList<Statistic>();
+				
+
+					while(weeks > 0) {
+						
+						if (weeks > 10) {
+						Statistic statistic = getUserStats(1, 1, fromDate, fromDate.plusWeeks(10));
+						stats.add(statistic);
+						weeks = weeks - 10;
+						fromDate = fromDate.plusWeeks(11);
+						} else {
+							Statistic statistic = getUserStats(1, 1, fromDate, toDate);
+							stats.add(statistic);
+							weeks = 0;
+						}
+						
+					}
+				
+				out.println(statisticsPageForm(stats));
+			} catch (Exception e) {
+				out.println(statisticsPageForm(null));
+				e.printStackTrace();
+			}
+		}
+		
+    }
+
+		
+	private String statisticsPageForm(List<Statistic> statistics) {
+			
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<body>");
+		sb.append("  <link rel=\"stylesheet\" type=\"text/css\" href=\"StyleSheets/StatisticsController.css\">\r\n" + 
+				"    <div class=\"wrapper\">\r\n" + 
+				"        <div class=\"\">\r\n" + 
+				"            <form id=\"filter_form\" >\r\n" + 
+				"                <div id=\"stat_title\">\r\n" + 
+				"                    <p id=\"stat_title_text\">STATISTICS</p>\r\n" + 
+				"                </div>\r\n" + 
+				"\r\n" + 
+				"                <div class=\"filter_row\">\r\n" + 
+				"                    <div>\r\n" + 
+				"                    <p class=\"descriptors\">Username</p>\r\n" + 
+				"                    <input class=\"credentials_rect\" type=\"text\" id=\"username\" name=\"username\" pattern=\"^[a-zA-Z0-9]*$\" title=\"Please enter letters and numbers only.\" maxlength=\"10\" placeholder=\"Search for user\" required><br>\r\n" + 
+				"                    </div>\r\n" + 
+				"                    <div>\r\n" + 
+				"                        <div>\r\n" + 
+				"                        <p class=\"descriptors\">From</p>\r\n" + 
+				"                        <p class=\"descriptors\" style=\"margin-left: 140px;\">To</p>\r\n" + 
+				"                    </div>\r\n" + 
+				"                    <div id=\"stat_date_picker\">\r\n" + 
+				"                    <input type=\"date\" id=\"from\" name=\"from\">\r\n" + 
+				"                    <input type=\"date\" id=\"to\" name=\"to\">\r\n" + 
+				"                </div>\r\n" + 
+				"            </div>\r\n" + 
+				"            <div>\r\n" + 
+				"                <p class=\"descriptors\">Activity</p>\r\n" + 
+				"                <div id=\"activity_picker\">\r\n" + 
+				"                    <select id=\"act_picker\" name=\"activity\" form=\"filter_form\">");
+		sb.append(getActivitySelectOptions());
+		sb.append("</select>");
+		sb.append("                </div>\r\n" + 
+				"            </div>\r\n" + 
+				"            <div>\r\n" + 
+				"                <p class=\"descriptors\">Role</p>\r\n" + 
+				"                <div id=\"activity_picker\">\r\n" + 
+				"                    <select id=\"act_picker\" name=\"role\" form=\"filter_form\">");
+		sb.append(getRoleSelectOptions());
+		sb.append("</select>");
+		sb.append("                </div>\r\n" + 
+				"            </div>\r\n" + 
+				"                <input class=\"submitBtn\" type=\"submit\" value=\"Search\">\r\n" + 
+				"                </div>\r\n" + 
+				"              </form> \r\n" + 
+				"        </div>\r\n");
+		
+		sb.append("<div>"); // Table goes here or nothingness goes here :(
+		if (statistics == null) {
+			sb.append("<p id=\"nothing\">There seems to be nothing here :(</p><br>");
+			sb.append("<p id=\"nothingSub\"> That could be because filter options are empty/incorrect or your filter options has yielded no results.</p>");
+		} else {
+			
+			
+			for (int i = 0; i < statistics.size(); i++) {
+				sb.append(getStatisticsDataTable(statistics.get(i)));
+			}
+			
+		}
+		sb.append("</div>");
+		
+		
+		
+		sb.append("</div>");
+		sb.append("</body>");
+		return sb.toString();
+	}
+	
+	/**
+	 * Gets the options in HTML format for the activities.
+	 * @return the HTML code for select options.
+	 */
+	private String getActivitySelectOptions() {
+		StringBuilder sbBuilder = new StringBuilder();
+		try {
+			List<ActivityType> activityTypes = dbService.getActivityTypes();
+			for (ActivityType activityType : activityTypes) {
+				sbBuilder.append("<option value=\"");
+				sbBuilder.append(activityType.getType());
+				sbBuilder.append("\">");
+				sbBuilder.append(activityType.getType());
+				sbBuilder.append("</option>\n");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return sbBuilder.toString();
 		
 	}
+	
+	/**
+	 * Gets the options in HTML format for the roles.
+	 * @return the HTML code for select options.
+	 */
+	private String getRoleSelectOptions() {
+		StringBuilder sbBuilder = new StringBuilder();
+		try {
+			List<Role> roles = dbService.getAllRoles();
+			for (Role role : roles) {
+				sbBuilder.append("<option value=\"");
+				sbBuilder.append(role.getRole());
+				sbBuilder.append("\">");
+				sbBuilder.append(role.getRole());
+				sbBuilder.append("</option>\n");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return sbBuilder.toString();
+	}
+	
+	
+	private String getStatisticsDataTable(Statistic statistic) {
+		StringBuilder sbBuilder = new StringBuilder();
+		
+		String[] rowLabel = statistic.getRowLabels();
+		
+		sbBuilder.append("<table style=\"margin-bottom:36px\" id=\"stats\">\n");
+		sbBuilder.append("<tr>\n");
+		sbBuilder.append("<th>Total</th>");
+		for (String lbl : statistic.getColumnLabels()) {
+			sbBuilder.append("<th>");
+			sbBuilder.append(lbl);
+			sbBuilder.append("</th>\n");
+		}
+		sbBuilder.append("<th>");
+		sbBuilder.append("Total");
+		sbBuilder.append("</th>\n");
+		sbBuilder.append("</tr>\n");
+		
+		int[][] data = statistic.getData();
+		int totalSum = 0;
+		
+		for (int i = 0; i < data.length; i++) {
+			int sum = 0;
+			
+			sbBuilder.append("<tr>\n");
+			
+				sbBuilder.append("<td>");
+				sbBuilder.append(rowLabel[i]);
+				sbBuilder.append("</td>\n");
+			
+			for (int j = 0; j < data[i].length; j++) {
+				sbBuilder.append("<td>");
+				sbBuilder.append(String.valueOf(data[i][j]));
+				sbBuilder.append("</td>\n");
+				sum += data[i][j];
+			}
+			sbBuilder.append("<td>");
+			sbBuilder.append(sum);
+			sbBuilder.append("</td>\n");
+			sbBuilder.append("</tr>\n");
+			totalSum += sum;
+		}
+		sbBuilder.append("<tr>");
+		sbBuilder.append("<td>");
+		sbBuilder.append("Total");
+		sbBuilder.append("</td>\n");
+		
+		int[] colSum = new int[data[0].length];
+		for (int i = 0; i < data.length; i++) {
+			 
+			for (int j = 0; j < data[i].length; j++) {
+				colSum[j] += data[i][j];
+			}
+		}
+		
+		for (int i : colSum) {
+			sbBuilder.append("<td>");
+			sbBuilder.append(String.valueOf(i));
+			sbBuilder.append("</td>\n");
+		}
+		
+		sbBuilder.append("<td>");
+		sbBuilder.append(String.valueOf(totalSum));
+		sbBuilder.append("</td>\n");
+		
+		sbBuilder.append("</tr>\n");
+		sbBuilder.append("</table>");
+		return sbBuilder.toString();
+	}
+	
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	doGet(req, resp);
+    }
+	
 
 }
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -17,11 +17,12 @@ import database.*;
 /**
  * Servlet implementation class UserController
  * 
- * A xx page. 
+ * A xx page.
  * 
  * Description of the class.
  * 
- * @author Ferit B�lezek ( Enter name if you've messed around with this file ;) )
+ * @author Ferit B�lezek ( Enter name if you've messed around with this file
+ *         ;) )
  * @version 1.0
  * 
  */
@@ -29,8 +30,7 @@ import database.*;
 @WebServlet("/UserPage")
 public class UserController extends servletBase {
 	private static final int PASSWORD_LENGTH = 6;
-	
-	
+
 	public UserController() {
 		super();
 	}
@@ -42,149 +42,211 @@ public class UserController extends servletBase {
 		out.println(getHeader());
 
 		String myName = "";
-		
+
 		User loggedInUser;
 		try {
 			loggedInUser = getLoggedInUser(req);
 			if (loggedInUser != null)
-	    		myName = (String)loggedInUser.getUsername();  // if the name exists typecast the name to a string
+				myName = (String) loggedInUser.getUsername(); // if the name exists typecast the name to a string
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}    	
-		
+		}
+
 		// check that the user is logged in
 		if (!isLoggedIn(req))
 			resp.sendRedirect("LogIn");
 		else
 
-			if (true) {
-				out.println("<h1>User Page " + "</h1>");
-				
-				// check if the administrator wants to add a new user in the form
-				String newName = req.getParameter("addname");
-				if (newName != null) {
-					if (checkNewName(newName)) {
-						boolean addPossible = addUser(newName);
-						if (!addPossible)
-							out.println("<p>Error: Suggested user name not possible to add</p>");
-					}	else
-						out.println("<p>Error: Suggesten name not allowed</p>");
-				}
+		if (true) {
+			out.println("<h1>User Page " + "</h1>");
 
-				// check if the administrator wants to delete a user by clicking the URL in the list
-				String deleteName = req.getParameter("deletename");
-				if (deleteName != null) {
-					if (checkNewName(deleteName)) {
-						deleteUser(deleteName);
-					}	else
-						out.println("<p>Error: URL wrong</p>");
+			// check if the administrator wants to add a new user in the form
+			String newName = req.getParameter("addname");
+			if (newName != null) {
+				if (checkNewName(newName)) {
+					boolean addPossible = addUser(newName);
+					if (!addPossible)
+						out.println("<p>Error: Suggested user name not possible to add</p>");
+				} else
+					out.println("<p>Error: Suggesten name not allowed</p>");
+			}
+
+			// check if the administrator wants to delete a user by clicking the URL in the
+			// list
+
+			String deleteName = req.getParameter("deletename");
+			if (deleteName != null) {
+				if (checkNewName(deleteName)) {
+					deleteUser(deleteName);
+				} else
+					out.println("<p>Error: URL wrong</p>");
+			}
+
+			String assignId = req.getParameter("selname");
+			String assignProject = req.getParameter("selproject");
+			String assignRole = req.getParameter("selrole");
+			if (assignId != null) {
+				int id = Integer.parseInt(assignId);
+				int proj =Integer.parseInt(assignProject);
+				int role = Integer.parseInt(assignRole);
+				addUserToProject(id, proj, role);
+			}
+			
+
+
+			try {
+				List<User> users = dbService.getAllUsers();
+				List<Project> projects = dbService.getAllProjects();
+				List<Role> roles = dbService.getAllRoles();
+				out.println("<p>Registered users:</p>");
+				out.println("<table border=" + addQuotes("1") + ">");
+				out.println("<tr><td>NAME</td><td></td><td>SELECT PROJECT</td><td>SELECT ROLE</td><td></td></tr>");
+				for (User u : users) {
+					String name = u.getUsername();
+					String deleteURL = "UserPage?deletename=" + name;
+					String deleteCode = "<a href=" + addQuotes(deleteURL) + " onclick="
+							+ addQuotes("return confirm('Are you sure you want to delete " + name + "?')")
+							+ "> delete </a>";
+					String projectList = "<form name="+ addQuotes("rList" + u.getUserId())+ "method=" + addQuotes("get")+
+							"> <input list=" + addQuotes("pList") + " name="
+							+ addQuotes("pList" + u.getUserId()) + "> <datalist id=" + addQuotes("pList") + ">";
+					for (Project p : projects) {
+						projectList += "<option value =" + addQuotes(p.getName()) + ">";
+					}
+					projectList += "</datalist> </form>";
+
+					String roleList = "<form name="+ addQuotes("rList" + u.getUserId())+ "method=" + addQuotes("get")+
+							"> <input list=" + addQuotes("rList") + "> <datalist id=" + addQuotes("rList") + ">";
+					for (Role r : roles) {
+						roleList += "<option value =" + addQuotes(r.getRole()) + ">";
+					}
+					roleList += "</datalist> </form>";
+					String assignURL = "UserPage?assignid=" + u.getUserId();
+					String assignCode = "<a href=" + addQuotes(assignURL) + " onclick="
+							+ addQuotes("pList" + u.getUserId()) + ".submit()> assign </a>";
+
+					if (u.isAdmin()) {
+						deleteCode = "";
+						projectList = "";
+						roleList = "";
+						assignCode = "";
+					}
+
+					out.println("<tr>");
+					out.println("<td>" + name + "</td>");
+					out.println("<td>" + deleteCode + "</td>");
+					out.println("<td>" + projectList + "</td>");
+					out.println("<td>" + roleList + "</td>");
+					out.println("<td>" + assignCode + "</td>");
+					out.println("</tr>");
 				}
-				
-				try {			    
-				    List<User> users = dbService.getAllUsers();
-				    out.println("<p>Registered users:</p>");
-				    out.println("<table border=" + addQuotes("1") + ">");
-				    out.println("<tr><td>NAME</td><td></td><td></td></tr>");
-				    for (User u : users) {
-				    	String name = u.getUsername();
-				    	String deleteURL = "UserPage?deletename="+name;
-				    	String deleteCode = "<a href=" + addQuotes(deleteURL) +
-				    			            " onclick="+addQuotes("return confirm('Are you sure you want to delete "+name+"?')") + 
-				    			            "> delete </a>";
-				    	String projectList = "UserPage?assignproject"+name;
-				    	String assignProjectCode = "<a href=" + addQuotes(projectURL) +
-				    						"onclick="addQuotes("")
-				    	
-				    	
-				    	if (u.isAdmin()) 
-				    		deleteCode = "";
-				    	out.println("<tr>");
-				    	out.println("<td>" + name + "</td>");
-				    	out.println("<td>" + deleteCode + "</td>");
-				    	out.println("</tr>");
-				    }
-				    out.println("</table>");
-				} catch (SQLException ex) {
-				    System.out.println("SQLException: " + ex.getMessage());
-				    System.out.println("SQLState: " + ex.getSQLState());
-				    System.out.println("VendorError: " + ex.getErrorCode());
-				}
-				out.println(addUserForm());
-				
-				out.println("<p><a href =" + addQuotes("functionality.html") + "> Functionality selection page </p>");
-				out.println("<p><a href =" + addQuotes("LogIn") + "> Log out </p>");
-				out.println("</body></html>");
-			} else {
-				
-			}				
-	}
-	
-	public String addUserForm() {
-    	String html;
-    	html = "<p> <form name=" + addQuotes("input");
-    	html += " method=" + addQuotes("get");
-    	html += "<p> Add user name: <input type=" + addQuotes("text") + " name=" + addQuotes("addname") + '>';    	
-    	html += "<input type=" + addQuotes("submit") + "value=" + addQuotes("Add user") + '>';
-    	html += "</form>";
-    	return html;
-	}
-	
-    
-    private boolean addUser(String name) {
-    	boolean resultOk = true;
-    	try{
-    		String newPassword = generatePassword();
-    		System.out.println(newPassword);
-    		User u = new User(0, name, newPassword, false);
-    		dbService.createUser(u);
-		} catch (Exception err) {
-		    resultOk = false;
-		    err.printStackTrace();
+				out.println("</table>");
+			} catch (SQLException ex) {
+				System.out.println("SQLException: " + ex.getMessage());
+				System.out.println("SQLState: " + ex.getSQLState());
+				System.out.println("VendorError: " + ex.getErrorCode());
+			}
+			out.println(addUserForm());
+			try {
+				out.println(assignUserForm());
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			out.println("<p><a href =" + addQuotes("functionality.html") + "> Functionality selection page </p>");
+			out.println("<p><a href =" + addQuotes("LogIn") + "> Log out </p>");
+			out.println("</body></html>");
+		} else {
+
 		}
-    	return resultOk;
-    }
-    
-    private boolean addUserToProject(int userId, int projectId, int roleId) {
-    	boolean resultOk = true;
-    	try {
-    		dbService.addUserToProject(userId, projectId, roleId);
-    	} catch (Exception err) {
-    		resultOk = false;
-    		err.printStackTrace();
-    	}
-    	return resultOk;
-    }
-    
-    private String generatePassword() {
-    	String result = "";
-    	Random r = new Random();
-    	for (int i=0; i<PASSWORD_LENGTH; i++)
-    		result += (char)(r.nextInt(26)+97); // 122-97+1=26
-    	return result;
-    }
-    
-    private void deleteUser(String name) {
-    	try{
+	}
+
+	public String addUserForm() {
+		
+		String html;
+		html = "<p> <form name=" + addQuotes("input");
+		html += " method=" + addQuotes("get");
+		html += "<p> Add user name: <input type=" + addQuotes("text") + " name=" + addQuotes("addname") + '>';
+		html += "<input type=" + addQuotes("submit") + "value=" + addQuotes("Add user") + '>';
+		html += "</form>";
+		return html;
+	}
+	
+	public String assignUserForm() throws SQLException {
+		List<User> users = dbService.getAllUsers();
+		List<Project> projects = dbService.getAllProjects();
+		List<Role> roles = dbService.getAllRoles();
+		String html;
+		html = "<p> Assign user to project. <form name="+addQuotes("assign")+" method ="+addQuotes("get")+
+				"<p> Name: <select name="+addQuotes("selname")+">";
+		for(User u:users) {
+			html += "<option value ="+ u.getUserId() + ">"+u.getUsername()+"</option>";
+		}
+		html += "</select> Project: <select name="+addQuotes("selproject")+">";
+		for(Project p:projects) {
+			html += "<option value =" + p.getProjectId() + ">"+p.getName()+"</option>";
+		}
+		html += "</select> Role: <select name="+addQuotes("selrole")+">";
+		for(Role r:roles) {
+			html += "<option value =" + r.getRoleId() + ">"+r.getRole()+"</option>";
+		}
+		html += "<input type="+ addQuotes("submit") + "value=" +addQuotes("Assign")+"> </form>";
+		return html;
+	}
+
+	private boolean addUser(String name) {
+		boolean resultOk = true;
+		try {
+			String newPassword = generatePassword();
+			System.out.println(newPassword);
+			User u = new User(0, name, newPassword, false);
+			dbService.createUser(u);
+		} catch (Exception err) {
+			resultOk = false;
+			err.printStackTrace();
+		}
+		return resultOk;
+	}
+
+	private boolean addUserToProject(int userId, int projectId, int roleId) {
+		boolean resultOk = true;
+		try {
+			dbService.addUserToProject(userId, projectId, roleId);
+		} catch (Exception err) {
+			resultOk = false;
+			err.printStackTrace();
+		}
+		return resultOk;
+	}
+
+	private String generatePassword() {
+		String result = "";
+		Random r = new Random();
+		for (int i = 0; i < PASSWORD_LENGTH; i++)
+			result += (char) (r.nextInt(26) + 97); // 122-97+1=26
+		return result;
+	}
+
+	private void deleteUser(String name) {
+		try {
 			dbService.deleteUserByUsername(name);
 		} catch (Exception err) {
-		    err.printStackTrace();
+			err.printStackTrace();
 		}
-    }
-    
+	}
 
-    private boolean checkNewName(String name) {
-    	int length = name.length();
-    	boolean isOk = (length>=5 && length<=10);
-    	if (isOk)
-    		for (int i=0; i<length; i++) {
-    			int ci = (int)name.charAt(i);
-    			boolean thisOk = ((ci>=48 && ci<=57) || 
-    					          (ci>=65 && ci<=90) ||
-    					          (ci>=97 && ci<=122));
-    			isOk = isOk && thisOk;
-    		}    	
-    	return isOk;
-    }
+	private boolean checkNewName(String name) {
+		int length = name.length();
+		boolean isOk = (length >= 5 && length <= 10);
+		if (isOk)
+			for (int i = 0; i < length; i++) {
+				int ci = (int) name.charAt(i);
+				boolean thisOk = ((ci >= 48 && ci <= 57) || (ci >= 65 && ci <= 90) || (ci >= 97 && ci <= 122));
+				isOk = isOk && thisOk;
+			}
+		return isOk;
+	}
 
 }
