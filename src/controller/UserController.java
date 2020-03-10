f228a2537d4c574ae9d2a141dcd052cd40a10454
ch0815la-s <ch0815la-s@egsws1970.pc.lu.se>
2020-03-10 11:52:08 +0100

    addUserFrom och import database

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import baseblocksystem.servletBase;
+import database.*;
 
 /**
  * Servlet implementation class UserController
@@ -23,6 +24,8 @@ import baseblocksystem.servletBase;
 
 @WebServlet("/UserPage")
 public class UserController extends servletBase {
+	
+	DatabaseService dbService;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,4 +34,18 @@ public class UserController extends servletBase {
 		//test comment test test hej hej
 		
 	}
+	
+	public String addUserForm() {
+		String html = "Enter username for new user";
+    	html += "<p> <form name=" + formElement("input");
+    	//html += " action=" + formElement(myURL); 
+    	html += " method=" + formElement("get");
+    	html += "<p> Name: <input type=" + formElement("text") + " name=" + formElement("user") + '>';
+    	html += "<p> <input type=" + formElement("submit") + "value=" + formElement("Submit") + '>';
+    	return html;
+	}
+	
+    private String formElement(String par) {
+    	return '"' + par + '"';
+    }
 }
