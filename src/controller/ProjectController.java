4e5c907eefff15c1c9656fc983dcf2287f1cf33d
gabtam <25223659+gabtam@users.noreply.github.com>
2020-03-11 23:16:44 +0100

    ProjectController updated

--- src/controller/ProjectController.java
+++ src/controller/ProjectController.java
@@ -25,7 +25,7 @@ import database.User;
  * 
  * Description of the class.
  * 
- * @author Ferit B�lezek ( Enter name if you've messed around with this file ;) )
+ * @author Ferit Bölezek ( Enter name if you've messed around with this file ;) )
  * @version 1.0
  * 
  */
@@ -54,16 +54,42 @@ public class ProjectController extends servletBase {
 				
 	}
 	
-	public boolean deleteProject(int projectId) {
+	public boolean deleteProject(int projectId) throws Exception {
+		if(dbService.getAllProjects().contains(dbService.getProject(projectId))) 
+		{
+			dbService.deleteProject(projectId);
+			return true;
+		}
 		return false;
 	}
 	
-	public boolean assignRole(User user, int projectId, int roleId) {
+	public boolean assignRole(User user, int projectId, int roleId) throws Exception {
+		if(dbService.getAllProjects().contains(dbService.getProject(projectId)) 
+				&& dbService.getAllUsers().contains(user)) 
+		{
+			for(int i=0; i< dbService.getAllRoles().size() ; i++) 
+			{
+				if(dbService.getAllRoles().get(i).getRoleId()==roleId) 
+				{
+					dbService.updateUserProjectRole(user.getUserId(), projectId, roleId);
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	
-	public Project createProject(String projectName) {
-		
+	public Project createProject(String projectName) throws SQLException {
+		int newId;
+		Project newProject;
+		newId = dbService.getAllProjects().get(dbService.getAllProjects().size()).getProjectId()+1;
+		newProject = new Project(newId, projectName);
+		if(!dbService.getAllProjects().contains(newProject)) 
+		{
+			dbService.createProject(new Project(newId, projectName));
+			return newProject;
+		}
+		return null;
 	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
